781c02a89be9e4ff28c73dc291e9e8f6017a1c67
Chinmaya Behera <chinmaya.behera@gyansys.com>
2025-04-07 12:54:27 +0530

    Added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello From Azure devops');
+    res.send('Hello From Github action');
 })
 
 app.listen(3000,()=>{
